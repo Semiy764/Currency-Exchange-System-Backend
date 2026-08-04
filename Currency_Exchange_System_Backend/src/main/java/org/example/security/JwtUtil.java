3d1a91fefec62616643d18a0719e20a8968395d9
unknown <symrfan@gmail.com>
2026-08-04 22:18:31 +0330

    add JwtUtil.java for generating token

---
+++ Currency_Exchange_System_Backend/src/main/java/org/example/security/JwtUtil.java
@@ -0,0 +1,71 @@
+package org.example.security;
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtUtil {
+
+    private static final String SECRET = "currency-exchange-super-secret-key-change-me-1234567890";
+    private static final long EXPIRATION_MS = 24 * 60 * 60 * 1000L;
+    private final SecretKey key = Keys.hmacShaKeyFor(SECRET.getBytes());
+
+    public String generateToken(int userId, String username, String role) {
+
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("username", username);
+        claims.put("role", role);
+
+        Date now = new Date();
+        Date expity = new Date(now.getTime() + EXPIRATION_MS);
+
+        return Jwts.builder()
+                .claims(claims)
+                .subject(String.valueOf(userId))
+                .issuedAt(now)
+                .expiration(expity)
+                .signWith(key)
+                .compact();
+    }
+
+
+    public Claims extractAllClaims(String token) {
+
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public long extractUserId(String token) {
+        return Long.parseLong(extractAllClaims(token).getSubject());
+    }
+
+    public String extractUsername(String token) {
+        return extractAllClaims(token).get("username", String.class);
+    }
+
+    public String getRole(String token) {
+        return extractAllClaims(token).get("role", String.class);
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            Claims claims = extractAllClaims(token);
+            return claims.getExpiration().after(new Date());
+        } catch (JwtException | IllegalArgumentException e) {
+            return  false;
+        }
+    }
+}
