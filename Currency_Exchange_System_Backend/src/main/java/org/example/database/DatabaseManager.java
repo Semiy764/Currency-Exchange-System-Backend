9248ced4ee384be1db4cffbe0f8e0bc16e0374fc
unknown <symrfan@gmail.com>
2026-08-04 21:26:20 +0330

    add DatabaseManager.java

---
+++ Currency_Exchange_System_Backend/src/main/java/org/example/database/DatabaseManager.java
@@ -0,0 +1,25 @@
+package org.example.database;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseManager {
+
+    private static final String DB_PATH = "database/exchange_project.db";
+    private static final String URL = "jdbc:sqlite:" + DB_PATH;
+
+
+    public static Connection getConnection() throws SQLException {
+
+        File dbFile = new File(DB_PATH);
+        File parentDir = dbFile.getParentFile();
+
+        if(parentDir != null && !parentDir.exists()) {
+            parentDir.mkdirs();
+        }
+
+        return DriverManager.getConnection(URL);
+    }
+}
