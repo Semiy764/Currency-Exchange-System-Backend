6b0ee171c0c6911b57573fe2dbe6ca3041eec925
unknown <symrfan@gmail.com>
2026-08-04 17:59:34 +0330

    add VaultLedger.java & LedgerReason enum

---
+++ Currency_Exchange_System_Backend/src/main/java/org/example/model/VaultLedger.java
@@ -0,0 +1,79 @@
+package org.example.model;
+
+import org.example.enums.LedgerReason;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class VaultLedger {
+
+    private long id;
+    private long currencyId;
+    private BigDecimal changeAmount;
+    private LedgerReason reason;
+    private LocalDateTime createdAt;
+    private long preformedByUserId;
+
+    public VaultLedger(BigDecimal changeAmount,
+                       LocalDateTime createdAt,
+                       long currencyId,
+                       long id,
+                       long preformedByUserId,
+                       LedgerReason reason) {
+
+        this.changeAmount = changeAmount;
+        this.createdAt = createdAt;
+        this.currencyId = currencyId;
+        this.id = id;
+        this.preformedByUserId = preformedByUserId;
+        this.reason = reason;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public BigDecimal getChangeAmount() {
+        return changeAmount;
+    }
+
+    public void setChangeAmount(BigDecimal changeAmount) {
+        this.changeAmount = changeAmount;
+    }
+
+    public long getCurrencyId() {
+        return currencyId;
+    }
+
+    public void setCurrencyId(long currencyId) {
+        this.currencyId = currencyId;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getPreformedByUserId() {
+        return preformedByUserId;
+    }
+
+    public void setPreformedByUserId(long preformedByUserId) {
+        this.preformedByUserId = preformedByUserId;
+    }
+
+    public LedgerReason getReason() {
+        return reason;
+    }
+
+    public void setReason(LedgerReason reason) {
+        this.reason = reason;
+    }
+}
