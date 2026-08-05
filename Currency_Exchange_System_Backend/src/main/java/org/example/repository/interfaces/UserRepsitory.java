0ca9dd00057c2c1c8bdba28ca72963596b0ec027
unknown <symrfan@gmail.com>
2026-08-05 16:39:30 +0330

    UserRepository methods was added

---
+++ Currency_Exchange_System_Backend/src/main/java/org/example/repository/interfaces/UserRepsitory.java
@@ -0,0 +1,23 @@
+package org.example.repository.interfaces;
+import org.example.model.User;
+import java.util.List;
+
+public interface UserRepsitory {
+    User save(User user);
+
+    User update(User user);
+
+    void delete(int userId);
+
+    User findById(int userId);
+
+    User findByUsername(String username);
+
+    List<User> findAll();
+
+    boolean existsByUsername(String username);
+
+    boolean existsById(int userId);
+
+    boolean existsByPhone(String phone);
+}
