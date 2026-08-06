a498251b1e6fac5f3078f3b6286bff9bd8c8c9b3
unknown <symrfan@gmail.com>
2026-08-06 17:18:00 +0330

    method save was added to TellerRepositoryImpl

--- Currency_Exchange_System_Backend/src/main/java/org/example/model/Teller.java
+++ Currency_Exchange_System_Backend/src/main/java/org/example/model/Teller.java
@@ -1,6 +1,8 @@
 package org.example.model;
 public class Teller extends UsersInfo {
 
+    public Teller(){}
+
     public Teller (String fullname, long id, String nationalId,long userId, String phoneNumber) {
         this.fullname = fullname;
         this.id = id;
---
+++ Currency_Exchange_System_Backend/src/test/java/Repository/TellerRepositoryTest.java
@@ -0,0 +1,22 @@
+package Repository;
+
+import org.example.App;
+import org.example.model.Teller;
+import org.example.repository.interfaces.TellerRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = App.class)
+public class TellerRepositoryTest {
+
+    @Autowired
+    private TellerRepository tellerRepository;
+
+    @Test
+    public void savingTellerTest() {
+        Teller teller = new Teller("Akbar asghari", "094948484", 7, "09234566857");
+        Teller savedTeller = tellerRepository.save(teller);
+        System.out.println(savedTeller.getId());
+    }
+}
