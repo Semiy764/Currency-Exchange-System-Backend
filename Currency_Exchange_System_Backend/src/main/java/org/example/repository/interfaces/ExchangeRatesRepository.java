74dfb0a3c63e7a752fd1212018e81ec71f9d6ed3
unknown <symrfan@gmail.com>
2026-08-07 18:06:49 +0330

    method findAllRates() was added to ExchangeRatesRepsotiroyImpl

--- Currency_Exchange_System_Backend/src/main/java/org/example/repository/interfaces/ExchangeRatesRepository.java
+++ Currency_Exchange_System_Backend/src/main/java/org/example/repository/interfaces/ExchangeRatesRepository.java
@@ -2,10 +2,13 @@ package org.example.repository.interfaces;
 
 import org.example.model.ExchangeRate;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface ExchangeRatesRepository {
 
     ExchangeRate save(ExchangeRate exchangeRate);
     List<ExchangeRate> findAll();
+    ExchangeRate findLastRate(int currencyId);
+//    List<ExchangeRate> findAllRatesOfCurrency(int currencyId);
 }
