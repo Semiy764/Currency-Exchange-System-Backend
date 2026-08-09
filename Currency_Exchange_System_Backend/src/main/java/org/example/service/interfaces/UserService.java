93fd3210a063c451812323f19345f11518da5bbf
unknown <symrfan@gmail.com>
2026-08-09 22:46:15 +0330

    method findAll() was added to UserServiceImpl

--- Currency_Exchange_System_Backend/src/main/java/org/example/service/interfaces/UserService.java
+++ Currency_Exchange_System_Backend/src/main/java/org/example/service/interfaces/UserService.java
@@ -2,9 +2,13 @@ package org.example.service.interfaces;
 
 import org.example.model.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(int userId);
     User findByUsername(String username);
+    List<User> findAll();
+//    List<User> findByRole();
 
 }
--- Currency_Exchange_System_Backend/src/test/java/service/AuthServiceTest.java
+++ Currency_Exchange_System_Backend/src/test/java/service/AuthServiceTest.java
@@ -103,4 +103,6 @@ public class AuthServiceTest {
 
         System.out.println(passwordEncoder.matches("Mohammad1022@", user.getPasswordHash()));
     }
+
+
 }
