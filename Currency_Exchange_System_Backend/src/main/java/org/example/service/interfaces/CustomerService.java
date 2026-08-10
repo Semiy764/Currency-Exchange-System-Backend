1af9c0a9c502510cbe1e83ec95a25bb32235b7d7
unknown <symrfan@gmail.com>
2026-08-10 17:58:13 +0330

    method findAll() was added to CustomerServiceImpl

--- Currency_Exchange_System_Backend/src/main/java/org/example/service/interfaces/CustomerService.java
+++ Currency_Exchange_System_Backend/src/main/java/org/example/service/interfaces/CustomerService.java
@@ -2,9 +2,12 @@ package org.example.service.interfaces;
 
 import org.example.model.Customer;
 
+import java.util.List;
+
 public interface CustomerService {
 
     Customer findById(int customerId);
 //    Customer findByNationalId(String nationalId);
     Customer findByUserId(int userId);
+    List<Customer> findAll();
 }
