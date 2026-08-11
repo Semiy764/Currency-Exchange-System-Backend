72e1bccc4e026252b68beeeb25a6eda203e31f66
unknown <symrfan@gmail.com>
2026-08-11 18:32:02 +0330

    method findAll() was added to CurrencyServiceImpl

--- Currency_Exchange_System_Backend/src/main/java/org/example/service/interfaces/CurrencyService.java
+++ Currency_Exchange_System_Backend/src/main/java/org/example/service/interfaces/CurrencyService.java
@@ -2,8 +2,11 @@ package org.example.service.interfaces;
 
 import org.example.model.Currency;
 
+import java.util.List;
+
 public interface CurrencyService {
 
     Currency addCurrency(Currency currency);
+    List<Currency> findAll();
 
 }
