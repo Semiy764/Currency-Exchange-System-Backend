80d112bf24258767d38c2e140cb1fe690cc6a16e
unknown <symrfan@gmail.com>
2026-08-12 03:19:28 +0330

    method recordEntry() was added to VaultLedgerRepositoryImpl

---
+++ Currency_Exchange_System_Backend/src/test/java/service/VaultLedgerServiceTest.java
@@ -0,0 +1,41 @@
+package service;
+
+import org.example.App;
+import org.example.enums.LedgerReason;
+import org.example.model.VaultBalance;
+import org.example.model.VaultLedger;
+import org.example.repository.interfaces.VaultLedgerRepository;
+import org.example.service.interfaces.VaultLedgerService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+@SpringBootTest(classes = App.class)
+public class VaultLedgerServiceTest {
+
+    @Autowired
+    private VaultLedgerService vaultLedgerService;
+
+    @Autowired
+    private VaultLedgerRepository vaultLedgerRepository;
+
+    @Test
+    public void savingVaultLedgerTest() {
+
+        VaultLedger vaultLedger = new VaultLedger(new BigDecimal(100), LocalDateTime.now(), 1, 1, LedgerReason.WITHDRAW);
+        vaultLedgerService.recordEntry(vaultLedger);
+
+        VaultLedger found = vaultLedgerRepository.findById(1);
+
+        assertThat(found.getId()).isEqualTo(1);
+        assertThat(found.getReason()).isEqualTo(LedgerReason.WITHDRAW);
+        assertThat(found.getCreatedAt()).isNotNull();
+        assertThat(found.getCurrencyId()).isEqualTo(1);
+        assertThat(found.getChangeAmount()).isEqualTo(new BigDecimal(100));
+    }
+}
