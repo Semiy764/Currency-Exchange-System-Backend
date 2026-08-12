ef7a2a286ea030a0171ec2960da4154b8463b327
unknown <symrfan@gmail.com>
2026-08-12 03:41:00 +0330

    method getHistoryBetween() was added to VaultLedgerServiceImpl

--- Currency_Exchange_System_Backend/src/main/java/org/example/service/impl/VaultLedgerServiceImpl.java
+++ Currency_Exchange_System_Backend/src/main/java/org/example/service/impl/VaultLedgerServiceImpl.java
@@ -7,6 +7,7 @@ import org.example.repository.interfaces.VaultLedgerRepository;
 import org.example.service.interfaces.VaultLedgerService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -42,5 +43,38 @@ public class VaultLedgerServiceImpl implements VaultLedgerService {
         }
 
         return vaultLedgerRepository.findByCurrencyIdOrderByCreatedAtDesc(currencyId);
+    }
+
+    @Override
+    public List<VaultLedger> getHistoryBetween(int currencyId, LocalDateTime start, LocalDateTime end) {
+
+        if(currencyId <= 0) {
+            throw new IllegalArgumentException("Please enter a valid number for currency id");
+        }
+
+        if(start == null) {
+            throw new IllegalArgumentException("start time cannot be null");
+        }
+
+        if(end == null) {
+            throw new IllegalArgumentException("end time cannot be null");
+        }
+
+        if(start.isAfter(end)) {
+            throw new IllegalArgumentException("start time is after the end");
+        }
+
+        LocalDateTime now = LocalDateTime.now();
+
+        if(start.isAfter(now)) {
+            throw new IllegalArgumentException("start time can not be in the future");
+        }
+
+        if(!currencyRepository.existsById(currencyId)) {
+            throw new ResourceNotFoundException("Currency not found with ID: " + currencyId);
+        }
+
+        return vaultLedgerRepository.findByCurrencyIdAndCreatedAtBetween(currencyId, start, end);
+
     }
 }
