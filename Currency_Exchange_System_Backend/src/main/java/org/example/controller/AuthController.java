4d794011cd573660cf41fb6ddae7d7daa9cebd51
unknown <symrfan@gmail.com>
2026-08-13 22:17:39 +0330

    api /register/register-teller was added to AuthController

---
+++ Currency_Exchange_System_Backend/src/main/java/org/example/controller/AuthController.java
@@ -0,0 +1,47 @@
+package org.example.controller;
+
+import org.example.dto.request.TellerRegisterRequest;
+import org.example.dto.response.AuthResponse;
+import org.example.model.User;
+import org.example.security.JwtUtil;
+import org.example.service.interfaces.AuthService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthService authService;
+    private final JwtUtil jwtUtil;
+
+
+    public AuthController(AuthService authService, JwtUtil jwtUtil) {
+        this.authService = authService;
+        this.jwtUtil = jwtUtil;
+    }
+
+
+    @PostMapping("/register/register-teller")
+    public AuthResponse registerTeller(@RequestBody TellerRegisterRequest request) {
+        User saved = authService.register(request);
+        return buildAuthResponse(saved);
+    }
+
+    private AuthResponse buildAuthResponse(User user) {
+
+        String token = jwtUtil.generateToken(
+                user.getId().intValue(),
+                user.getUsername(),
+                user.getRole().name()
+        );
+
+        return new AuthResponse(
+                user.getRole(),
+                token, user.getId(),
+                user.getUsername());
+
+    }
+}
