ee8d9a75c0aa27bd726c10e6f50e28666bdace92
unknown <symrfan@gmail.com>
2026-08-17 18:24:51 +0330

    method getAllCurrentRates() was added to ExchangeRateServiceImpl

--- Currency_Exchange_System_Backend/src/main/java/org/example/service/interfaces/ExchangeRateService.java
+++ Currency_Exchange_System_Backend/src/main/java/org/example/service/interfaces/ExchangeRateService.java
@@ -3,9 +3,11 @@ package org.example.service.interfaces;
 import org.example.model.ExchangeRate;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public interface ExchangeRateService {
 
     ExchangeRate setRate(int currencyId, BigDecimal buyRate, BigDecimal sellRate, int createdByUserId);
     ExchangeRate getCurrentRate(int currencyId);
+    List<ExchangeRate> getAllCurrentRates();
 }
