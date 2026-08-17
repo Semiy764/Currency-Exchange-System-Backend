982ba3af4476f9f84e96d0d264cdba6b363870af
unknown <symrfan@gmail.com>
2026-08-17 22:55:12 +0330

    GET api/transactions was added to TransactionController

--- Currency_Exchange_System_Backend/src/main/java/org/example/controller/TransactionController.java
+++ Currency_Exchange_System_Backend/src/main/java/org/example/controller/TransactionController.java
@@ -8,13 +8,11 @@ import org.example.security.AuthenticatedUser;
 import org.example.service.interfaces.TransactionService;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/transactions")
@@ -69,9 +67,21 @@ public class TransactionController {
 
     }
 
+    @GetMapping
+    public List<Transaction> getAllTransactions(@AuthenticationPrincipal AuthenticatedUser principal) {
+        isAdmin(principal);
+        return transactionService.findAllOrderByCreatedAtDesc();
+    }
+
     private void isAdminOrTeller(AuthenticatedUser principal) {
         if(!"ADMIN".equals(principal.role()) && !"TELLER".equals(principal.role())) {
             throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Admin or Teller only");
+        }
+    }
+
+    private void isAdmin(AuthenticatedUser principal) {
+        if(!"ADMIN".equals(principal.role())) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Admin only");
         }
     }
 }
