8757413ff07131db32de5040c46cef9ff16979cf
unknown <symrfan@gmail.com>
2026-08-17 19:37:41 +0330

    GET api/customres was added to CustomerController

---
+++ Currency_Exchange_System_Backend/src/main/java/org/example/controller/CustomerController.java
@@ -0,0 +1,40 @@
+package org.example.controller;
+
+import org.example.model.Customer;
+import org.example.security.AuthenticatedUser;
+import org.example.service.interfaces.CustomerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/customers")
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+
+    @GetMapping
+    public List<Customer> getAllCustomers(@AuthenticationPrincipal AuthenticatedUser principal) {
+        return customerService.findAll();
+    }
+
+
+    private void isAdminOrTeller(AuthenticatedUser principal) {
+
+        if(!"ADMIN".equals(principal.role()) && !"TELLER".equals(principal.role())) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Admin or Teller only");
+        }
+    }
+
+
+}
