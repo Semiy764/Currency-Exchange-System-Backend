75e46f3b6a854857ee6db4e2210b7e267ffba525
unknown <symrfan@gmail.com>
2026-08-17 22:46:43 +0330

    POST api/transactions/buy was added to TransactionController

---
+++ Currency_Exchange_System_Backend/src/main/java/org/example/dto/request/AdminOrTellerTransactionRequest.java
@@ -0,0 +1,66 @@
+package org.example.dto.request;
+
+import org.example.enums.TxStatus;
+import org.example.enums.TxType;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class AdminOrTellerTransactionRequest {
+    private Long currencyId;
+    private Long customerId;
+    private BigDecimal amountCurrency;
+    private BigDecimal amountToman;
+    private BigDecimal requestedRate;
+    private BigDecimal rateUsed;
+
+    public BigDecimal getRequestedRate() {
+        return requestedRate;
+    }
+
+    public void setRequestedRate(BigDecimal requestedRate) {
+        this.requestedRate = requestedRate;
+    }
+
+    public BigDecimal getAmountCurrency() {
+        return amountCurrency;
+    }
+
+    public void setAmountCurrency(BigDecimal amountCurrency) {
+        this.amountCurrency = amountCurrency;
+    }
+
+    public BigDecimal getAmountToman() {
+        return amountToman;
+    }
+
+    public void setAmountToman(BigDecimal amountToman) {
+        this.amountToman = amountToman;
+    }
+
+    public Long getCurrencyId() {
+        return currencyId;
+    }
+
+    public void setCurrencyId(Long currencyId) {
+        this.currencyId = currencyId;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public BigDecimal getRateUsed() {
+        return rateUsed;
+    }
+
+    public void setRateUsed(BigDecimal rateUsed) {
+        this.rateUsed = rateUsed;
+    }
+
+
+}
