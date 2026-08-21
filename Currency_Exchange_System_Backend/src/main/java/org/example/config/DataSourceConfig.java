cba0157cebfa0bc269bc3a849e69592240d709dc
unknown <symrfan@gmail.com>
2026-08-21 12:06:20 +0330

    some changes was added to database paths

--- Currency_Exchange_System_Backend/src/main/java/org/example/config/DataSourceConfig.java
+++ Currency_Exchange_System_Backend/src/main/java/org/example/config/DataSourceConfig.java
@@ -1,5 +1,8 @@
 package org.example.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.example.database.DatabaseManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
@@ -9,6 +12,18 @@ import javax.sql.DataSource;
 
 @Configuration
 public class DataSourceConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        String url = "jdbc:sqlite:" + DatabaseManager.getDbPath();
+
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(url);
+        config.setDriverClassName("org.sqlite.JDBC");
+        config.setMaximumPoolSize(1); // sqlite فقط یک writer هم‌زمان قبول می‌کنه
+
+        return new HikariDataSource(config);
+    }
 
     @Bean
     public PlatformTransactionManager transactionManager(DataSource dataSource) {
