5bda053b8bab5e3282e74472246c253f3fcd01be
unknown <symrfan@gmail.com>
2026-09-11 11:14:03 +0330

    preauthorize was added to TellerController

--- Currency_Exchange_System_Backend/src/main/java/org/example/controller/TellerController.java
+++ Currency_Exchange_System_Backend/src/main/java/org/example/controller/TellerController.java
@@ -3,14 +3,12 @@ package org.example.controller;
 import org.example.model.Teller;
 import org.example.security.AuthenticatedUser;
 import org.example.service.interfaces.TellerService;
-import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
-
 import java.util.List;
 
 @RestController
@@ -24,17 +22,14 @@ public class TellerController {
     }
 
     @GetMapping
-    public List<Teller> findAllTellers(@AuthenticationPrincipal AuthenticatedUser principal) {
-
-        isAdmin(principal);
+    @PreAuthorize("hasRole('ADMIN')")
+    public List<Teller> findAllTellers() {
         return tellerService.findAll();
     }
 
     @GetMapping("/{tellerId}")
-    public Teller findTeller(@PathVariable int tellerId,
-                           @AuthenticationPrincipal AuthenticatedUser principal) {
-
-        isAdmin(principal);
+    @PreAuthorize("hasRole('ADMIN')")
+    public Teller findTeller(@PathVariable int tellerId) {
         return tellerService.findById(tellerId);
     }
 
@@ -43,10 +38,4 @@ public class TellerController {
         return tellerService.findByUserId(principal.id());
     }
 
-    private void isAdmin(AuthenticatedUser principal) {
-
-        if(!"ADMIN".equals(principal.role())) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Admin only");
-        }
-    }
 }
